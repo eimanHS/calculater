39e4bb9e1a5493ff8ce69e654267998ba3c7e8e3
acer <acer@10.0.0.6>
2018-02-09 09:47:55 -0800

    commit only

--- src/calculater/First_test.java
+++ src/calculater/First_test.java
@@ -12,16 +12,6 @@ import org.junit.jupiter.api.Test;
 class First_test {
 	Controler c1 ;
 	Model m1;
-	
-	
-	
-//	@BeforeAll
-//	static void setUpBeforeClass() throws Exception {
-//	}
-
-//	@AfterAll
-//	static void tearDownAfterClass() throws Exception {
-//	}
 
 	@BeforeEach
 	void setUp() throws Exception {
