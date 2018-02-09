8a667774f6042e4a879e25becc68aa348b445a51
acer <acer@10.0.0.6>
2018-02-08 23:52:26 -0800

    did not test yet

---
+++ src/calculater/First_test.java
@@ -0,0 +1,42 @@
+package calculater;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class First_test {
+	Controler c1 ;
+	Model m;
+	
+	
+	
+//	@BeforeAll
+//	static void setUpBeforeClass() throws Exception {
+//	}
+
+//	@AfterAll
+//	static void tearDownAfterClass() throws Exception {
+//	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	      c1 = new Controler();
+	      m = new Model();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	void test() {
+	assertEquals(5,m.add(3,2));
+	}
+
+}
